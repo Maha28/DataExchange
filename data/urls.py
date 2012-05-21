d9a4d3a60f5aa606ed2369ce4bd411d975717d23
Maher Manoubi <maher.manoubi@gmail.com>
2012-05-21 11:44:00 -0400

    added connection to the source page

--- data/urls.py
+++ data/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     url(r'^$', 'data.views.home', name='home'),
+    url(r'source/$', 'data.views.source', name='source'),
 )
