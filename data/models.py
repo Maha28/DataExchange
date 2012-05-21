f6e01dbe0ece7f3446759d5422770614a321ee61
Maher Manoubi <maher.manoubi@gmail.com>
2012-05-20 20:13:03 -0400

    Added the code for models

--- data/models.py
+++ data/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Source1:
+    A = models.CharField(max_length=5)
+    B = models.CharField(max_length=5)
+    
+class Source2:
+    C = models.CharField(max_length=5)
+    D = models.CharField(max_length=5)   
+    
+class Source2:
+    E = models.CharField(max_length=5)
+    F = models.CharField(max_length=5)       
+    
+class Mapping:
+    S = models.CharField(max_length=5)
+    T = models.CharField(max_length=5)      
+    
+class Target1:
+    A = models.CharField(max_length=5)
+    B = models.CharField(max_length=5)      
+    
+class Target2:
+    C = models.CharField(max_length=5)
+    D = models.CharField(max_length=5)           
