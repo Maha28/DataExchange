5c50461ed1e3ecb31ae35b0d48aa57141d65b0e4
Maher Manoubi <maher.manoubi@gmail.com>
2012-05-22 22:52:02 -0400

    Added the controllers of mapping

--- data/urls.py
+++ data/urls.py
@@ -8,4 +8,6 @@ urlpatterns = patterns('',
     url(r'database/$', 'data.views.database', name='database'),
     url(r'database/populate_source$', 'data.views.populate_source', name='populate_source'),
     url(r'database/clear_source$', 'data.views.clear_source', name='clear_source'),
+    url(r'database/populate_mapping$', 'data.views.populate_mapping', name='populate_mapping'),
+    url(r'database/clear_mapping$', 'data.views.clear_mapping', name='clear_mapping'),    
 )
