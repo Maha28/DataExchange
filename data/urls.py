1d3b691eec2182a04f0f8b392138ba27b20f7726
Maher Manoubi <maher.manoubi@gmail.com>
2012-06-15 01:21:08 -0400

    Cleaned up views entirely

--- data/urls.py
+++ data/urls.py
@@ -6,31 +6,24 @@ urlpatterns = patterns('',
                        
     #Database
     url(r'^database/$', 'data.views.database', name='database'),
-    url(r'^database/populate_source/$', 'data.views.populate_source', name='populate_source'),
-    url(r'^database/clear_source/$', 'data.views.clear_source', name='clear_source'),
-    url(r'^database/populate_mapping/$', 'data.views.populate_mapping', name='populate_mapping'),
-    url(r'^database/clear_mapping/$', 'data.views.clear_mapping', name='clear_mapping'),    
-    url(r'^database/generate_equal/$', 'data.views.generate_equal', name='generate_equal'),
-    url(r'^database/clear_equal/$', 'data.views.clear_equal', name='clear_equal'), 
-    url(r'^database/clear_all/$', 'data.views.clear_all', name='clear_all'),                     
+    url(r'^database/populate_source/(?P<view_name>\w+)/$', 'data.views.populate_source', name='populate_source'),
+    url(r'^database/clear_source/(?P<view_name>\w+)/$', 'data.views.clear_source', name='clear_source'),
+    url(r'^database/populate_mapping/(?P<view_name>\w+)/$', 'data.views.populate_mapping', name='populate_mapping'),
+    url(r'^database/clear_mapping/(?P<view_name>\w+)/$', 'data.views.clear_mapping', name='clear_mapping'),    
+    url(r'^database/generate_equal/(?P<view_name>\w+)/$', 'data.views.generate_equal', name='generate_equal'),
+    url(r'^database/clear_equal/(?P<view_name>\w+)/$', 'data.views.clear_equal', name='clear_equal'), 
+    url(r'^database/update_mapping_from_equal/(?P<view_name>\w+)/$', 'data.views.update_mapping_from_equal', name='update_mapping_from_equal'),  
+    url(r'^database/update_source_from_equal/(?P<view_name>\w+)/$', 'data.views.update_source_from_equal', name='update_source_from_equal'),        
+    url(r'^database/clear_all/(?P<view_name>\w+)/$', 'data.views.clear_all', name='clear_all'),                     
     
     #Source                   
     url(r'^source/$', 'data.views.source', name='source'),
-    url(r'^source/populate_source/$', 'data.views.populate_source_from_source', name='populate_source_from_source'),
-    url(r'^source/clear_source/$', 'data.views.clear_source_from_source', name='clear_source_from_source'),
-    
     #Mapping
     url(r'^mapping/$', 'data.views.mapping', name='mapping'),
-    url(r'^mapping/populate_mapping/$', 'data.views.populate_mapping_from_mapping', name='populate_mapping_from_mapping'),
-    url(r'^mapping/clear_mapping/$', 'data.views.clear_mapping_from_mapping', name='clear_mapping_from_mapping'),  
-    
+
     #Equal    
     url(r'^equal/$', 'data.views.equal', name='equal'),    
-    url(r'^equal/generate_equal/$', 'data.views.generate_equal_from_equal', name='generate_equal_from_equal'),  
-    url(r'^equal/update_mapping/$', 'data.views.update_mapping_from_equal', name='update_mapping_from_equal'),  
-    url(r'^equal/update_source/$', 'data.views.update_source_from_equal', name='update_source_from_equal'),        
-        url(r'^equal/clear_equal/$', 'data.views.clear_equal_from_equal', name='clear_equal_from_equal'),   
+  
     #Target   
-    url(r'^target/$', 'data.views.target', name='target'),
-       
+    url(r'^target/$', 'data.views.target', name='target'),   
 )
--- data/views.py
+++ data/views.py
@@ -4,9 +4,11 @@ from django.core.urlresolvers import resolve, reverse
 from django.shortcuts import render
 from data import models
 
+#Home
 def home(request):
     return render(request, 'home.html')
 
+#Source
 def source(request):
     context = {}
     context['source1'] = models.Source1.objects.all()
@@ -14,98 +16,93 @@ def source(request):
     context['source3'] = models.Source3.objects.all()  
     return render(request, 'source.html', context)
 
+def populate_source(request, view_name):
+    models.Source1.objects.populate_source()
+    messages.success(request, "You have successfully populated the source tables")
+    if view_name == 'source':
+        return HttpResponseRedirect(reverse('source'))  
+    else:     
+        return HttpResponseRedirect(reverse('database'))  
+
+def clear_source(request, view_name):
+    models.Source1.objects.clear_source()
+    messages.success(request, "You have successfully cleared the source tables")
+    if view_name == 'source':
+        return HttpResponseRedirect(reverse('source'))  
+    else:     
+        return HttpResponseRedirect(reverse('database'))   
+
+#Mapping
 def mapping(request):
     context = {}
     context['mapping'] = models.Mapping.objects.all() 
     return render(request, 'mapping.html',context)
 
+def populate_mapping(request,view_name):
+    models.Mapping.objects.populate_mapping()
+    messages.success(request, "You have successfully populated the mapping tables")
+    if view_name == 'mapping':
+        return HttpResponseRedirect(reverse('mapping'))  
+    else:     
+        return HttpResponseRedirect(reverse('database'))     
+
+def clear_mapping(request,view_name):
+    models.Mapping.objects.clear_mapping()
+    messages.success(request, "You have successfully cleared the mapping tables")
+    if view_name == 'mapping':
+        return HttpResponseRedirect(reverse('mapping'))  
+    else:     
+        return HttpResponseRedirect(reverse('database'))
+#Target
+def target(request):
+    return render(request, 'target.html')
+
+#Equal
 def equal(request):
     context = {}
     context['dom'] = models.Equal.objects.get_dom()
     context['equal'] = models.Equal.objects.all()
     return render(request, 'equal.html',context)
 
-def target(request):
-    return render(request, 'target.html')
-
-def database(request):
-    return render(request, 'database.html')
-
-def populate_source(request):
-    models.Source1.objects.populate_source()
-    messages.success(request, "You have successfully populated the source tables")
-    return HttpResponseRedirect(reverse('database'))  
-
-def clear_source(request):
-    models.Source1.objects.clear_source()
-    messages.success(request, "You have successfully cleared the source tables")
-    return HttpResponseRedirect(reverse('database'))    
-
-def populate_mapping(request):
-    models.Mapping.objects.populate_mapping()
-    messages.success(request, "You have successfully populated the mapping tables")
-    return HttpResponseRedirect(reverse('database'))    
-
-def clear_mapping(request):
-    models.Mapping.objects.clear_mapping()
-    messages.success(request, "You have successfully cleared the mapping tables")
-    return HttpResponseRedirect(reverse('database'))  
-
-def generate_equal(request):
+def generate_equal(request,view_name):
     models.Equal.objects.generate_equal()
     messages.success(request, "You have successfully generated Equal")
-    return HttpResponseRedirect(reverse('database'))    
+    if view_name == 'equal':
+        return HttpResponseRedirect(reverse('equal'))  
+    else:     
+        return HttpResponseRedirect(reverse('database'))  
 
-def clear_equal(request):
+def update_mapping_from_equal(request,view_name):
+    models.Equal.objects.rule_13()
+    messages.success(request, "You have successfully updated the mapping table using equal")
+    if view_name == 'equal':
+        return HttpResponseRedirect(reverse('equal'))  
+    else:     
+        return HttpResponseRedirect(reverse('database'))  
+
+def update_source_from_equal(request,view_name):
+    models.Equal.objects.rule_14()
+    messages.success(request, "You have successfully updated the source table using equal")
+    if view_name == 'equal':
+        return HttpResponseRedirect(reverse('equal'))  
+    else:     
+        return HttpResponseRedirect(reverse('database'))
+
+def clear_equal(request,view_name):
     models.Equal.objects.clear_equal()
     messages.success(request, "You have successfully cleared Equal")
-    return HttpResponseRedirect(reverse('database'))  
+    if view_name == 'equal':
+        return HttpResponseRedirect(reverse('equal'))  
+    else:     
+        return HttpResponseRedirect(reverse('database'))
+
+#Database
+def database(request):
+    return render(request, 'database.html')
 
 def clear_all(request):
     models.Source1.objects.clear_source()
     models.Mapping.objects.clear_mapping()
     models.Equal.objects.clear_equal()
     messages.success(request, "You have successfully cleared everything")
-    return HttpResponseRedirect(reverse('database'))  
-
-#The following methods are temporary copied to accomplish a small task.
-#TODO: Use arguments to combine these views with the old ones
-def populate_source_from_source(request):
-    models.Source1.objects.populate_source()
-    messages.success(request, "You have successfully populated the source tables")
-    return HttpResponseRedirect(reverse('source'))    
-
-def clear_source_from_source(request):
-    models.Source1.objects.clear_source()
-    messages.success(request, "You have successfully cleared the source tables")
-    return HttpResponseRedirect(reverse('source')) 
-
-def populate_mapping_from_mapping(request):
-    models.Mapping.objects.populate_mapping()
-    messages.success(request, "You have successfully populated the mapping tables")
-    return HttpResponseRedirect(reverse('mapping'))    
-
-def clear_mapping_from_mapping(request):
-    models.Mapping.objects.clear_mapping()
-    messages.success(request, "You have successfully cleared the mapping tables")
-    return HttpResponseRedirect(reverse('mapping'))  
-
-def generate_equal_from_equal(request):
-    models.Equal.objects.generate_equal()
-    messages.success(request, "You have successfully generated Equal")
-    return HttpResponseRedirect(reverse('equal'))   
-
-def clear_equal_from_equal(request):
-    models.Equal.objects.clear_equal()
-    messages.success(request, "You have successfully cleared Equal")
-    return HttpResponseRedirect(reverse('equal'))  
-
-def update_mapping_from_equal(request):
-    models.Equal.objects.rule_13()
-    messages.success(request, "You have successfully updated the mapping table using equal")
-    return HttpResponseRedirect(reverse('equal'))  
-
-def update_source_from_equal(request):
-    models.Equal.objects.rule_14()
-    messages.success(request, "You have successfully updated the source table using equal")
-    return HttpResponseRedirect(reverse('equal')) 
+    return HttpResponseRedirect(reverse('database')) 
